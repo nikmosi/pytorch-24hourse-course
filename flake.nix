d833ae665e2777aff3ee06c44ac783dcba6ee4ee
nikmosi <i@nikmosi.ru>
2025-03-08 21:13:41 +0700

    add dev shell
    add dev shell
    
    lock

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { self, nixpkgs, ... }@args:
+    args.flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages =
+            with pkgs;
+            [
+              gcc
+              python312
+              uv
+            ]
+            ++ (with python312Packages; [
+            ]);
+          shellHook = ''exec fish'';
+        };
+      }
+    );
+}
